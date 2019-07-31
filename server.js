aaa3fb7079745493919269b7ce028fec1d9ee24b
Max <18743976+YoKoGFX@users.noreply.github.com>
2019-07-31 16:54:13 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
+var compression = require('compression');
 var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
+
+app.use(compression());
 
 users = [];
 connections = [];
