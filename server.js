563ebc413d09b0a3a8d910f59543bd03e5a32173
Max <18743976+YoKoGFX@users.noreply.github.com>
2019-07-31 15:11:53 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 var express = require('express');
-var app = express();
+// var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
 
-const compression = require('compression');
-const app = express();
-app.use(compression());
+var compression = require('compression')
+var express = require('express')
+
+var app = express()
+app.use(compression({ filter: shouldCompress }))
+
+function shouldCompress (req, res) {
+  if (req.headers['x-no-compression']) {
+    // don't compress responses with this request header
+    return false
+  }
+
+  // fallback to standard filter function
+  return compression.filter(req, res)
+}
 
 
 users = [];
